fb682fdf00a9983fc9a620f908f36f76c18159a5
Gerry Sihaj <gsihaj5@gmail.com>
2021-12-01 21:07:26 +0700

    ambient

--- WEBGL/Geometry.js
+++ WEBGL/Geometry.js
@@ -1,58 +1,95 @@
-import Vector3 from "./Vector3.js";
-import Color from "./Color.js";
+import Vector3 from './Vector3.js'
+import Color from './Color.js'
 
 export default class Geometry {
-    _vertices = []; // Vector3
-    _faces = [];
+	_vertices = [] // Vector3
+	_faces = []
+	_normals = []
 
-    //position :Vector3
-    constructor(position = new Vector3(0, 0, 0), color = new Color(255, 0, 0)) {
-        this.position = position
-        this.color = color;
-    }
+	//position :Vector3
+	constructor (position = new Vector3(0, 0, 0), color = new Color(255, 0, 0)) {
+		this.position = position
+		this.color = color
+	}
 
-    //Vector3
-    addVertice(vertice) {
-        this._vertices.push(vertice)
-    }
+	_calculateNormals (vertices) {
+		for (let vertice_index = 0; vertice_index < vertices.length; vertice_index += 3) {
+			let vertice1 = vertices[vertice_index]
+			let vertice2 = vertices[vertice_index + 1]
+			let vertice3 = vertices[vertice_index + 2]
 
-    addFace(face) {
-        this._faces.push(face)
-    }
+			let vector1 = new Vector3(vertice2.x - vertice1.x, vertice2.y - vertice1.y, vertice2.z - vertice1.z)
+			let vector2 = new Vector3(vertice3.x - vertice1.x, vertice3.y - vertice1.y, vertice3.z - vertice1.z)
 
-    getColorArray() {
-        let colors = [];
+			let normal = new Vector3(
+				1000000 * ((vector2.y * vector1.z) - (vector2.z * vector1.y)),
+				1000000 * ((vector2.z * vector1.x) - (vector2.x * vector1.z)),
+				1000000 * ((vector2.x * vector1.y) - (vector2.y * vector1.x)),
+			)
+			this.addNormals(normal)
+			this.addNormals(normal)
+			this.addNormals(normal)
+		}
+	}
 
-        this._faces.forEach((faces) => {
-            faces.getArray().forEach((index) => colors.push(...this.color.getArray()))
-        })
-        return colors
-    }
+	//Vector3
+	addVertice (vertice) {
+		this._vertices.push(vertice)
+	}
 
-    getVertices() {
-        let vertices = [];
+	addFace (face) {
+		this._faces.push(face)
+	}
 
-        this._faces.forEach((faces) => {
-            faces.getArray().forEach((index) => vertices.push(this._vertices[index]))
-        })
+	addNormals (normal) {
+		this._normals.push(normal)
+	}
 
-        return vertices
-    }
+	getColorArray () {
+		let colors = []
 
-    getVerticeArray() {
-        let vertices = [];
+		this._faces.forEach((faces) => {
+			faces.getArray().forEach((index) => colors.push(...this.color.getArray()))
+		})
+		return colors
+	}
 
-        this._faces.forEach((faces) => {
-            faces.getArray().forEach((index) => vertices.push(...this._vertices[index].getArray()))
-        })
+	getVertices () {
+		let vertices = []
 
-        return vertices
-    }
+		this._faces.forEach((faces) => {
+			faces.getArray().forEach((index) => vertices.push(this._vertices[index]))
+		})
 
-    translate(x, y, z) {
-        this.position.x += x;
-        this.position.y += y;
-        this.position.z += z;
-    }
+		return vertices
+	}
+
+	getNormalArray () {
+		let faceNormals = []
+
+		this._normals.forEach(normal => faceNormals.push(...normal.getArray()))
+		console.log(this._normals)
+		console.log(faceNormals)
+
+
+		return faceNormals
+	}
+
+	getVerticeArray () {
+		console.log('get vertice')
+		let vertices = []
+
+		this._faces.forEach((faces) => {
+			faces.getArray().forEach((index) => vertices.push(...this._vertices[index].getArray()))
+		})
+
+		return vertices
+	}
+
+	translate (x, y, z) {
+		this.position.x += x
+		this.position.y += y
+		this.position.z += z
+	}
 
 }
--- WEBGL/Scene.js
+++ WEBGL/Scene.js
@@ -1,136 +1,164 @@
 export default class Scene {
-    geometries = [];
+	geometries = []
 
-    constructor(domElement) {
-        this.domElement = domElement;
-        this.context = domElement.getContext("webgl");
+	constructor (domElement) {
+		this.domElement = domElement
+		this.context = domElement.getContext('webgl')
 
-        if (this.context === null) {
-            alert("Unable to initialize WebGL. Your browser or machine may not support it.");
-            return;
-        }
-        this._createShaderProgram()
-    }
+		if (this.context === null) {
+			alert('Unable to initialize WebGL. Your browser or machine may not support it.')
+			return
+		}
+		this._createShaderProgram()
+	}
 
-    _createShaderProgram() {
-        this.shaderProgram = this.context.createProgram()
-        this.context.attachShader(this.shaderProgram, this._createVertexShader())
-        this.context.attachShader(this.shaderProgram, this._createFragmentShader())
-        this.context.linkProgram(this.shaderProgram)
-        this.context.useProgram(this.shaderProgram)
-    }
+	_createShaderProgram () {
+		this.shaderProgram = this.context.createProgram()
+		this.context.attachShader(this.shaderProgram, this._createVertexShader())
+		this.context.attachShader(this.shaderProgram, this._createFragmentShader())
+		this.context.linkProgram(this.shaderProgram)
+		this.context.useProgram(this.shaderProgram)
+	}
 
-    _createVertexShader() {
-
-        let vertexShaderCode = `
+	_createVertexShader () {
+		let vertexShaderCode = `
             attribute vec3 aCoordinates;
             attribute vec3 aColors;
+            attribute vec3 aNormal;
             uniform mat4 uProjectionMatrix;
             uniform mat4 uViewMatrix;
-            varying mediump vec4 vColor;
+            varying vec4 vColor;
+            varying vec3 vNormal;
             void main(){
                 gl_Position =  uProjectionMatrix * uViewMatrix *  vec4(aCoordinates.x / 100.0, aCoordinates.y / 100.0, aCoordinates.z / 100.0, 1.0);
                 vColor = vec4(aColors, 1);
+                
             }`
 
-        let vertexShader = this.context.createShader(this.context.VERTEX_SHADER);
-        this.context.shaderSource(vertexShader, vertexShaderCode)
-        this.context.compileShader(vertexShader)
+		let vertexShader = this.context.createShader(this.context.VERTEX_SHADER)
+		this.context.shaderSource(vertexShader, vertexShaderCode)
+		this.context.compileShader(vertexShader)
 
-        return vertexShader;
-    }
+		let compiled = this.context.getShaderParameter(vertexShader, this.context.COMPILE_STATUS)
+		if (!compiled) {
+			console.error(this.context.getShaderInfoLog(vertexShader))
+		}
+		return vertexShader
+	}
 
-    _createFragmentShader() {
-        let fragmentShaderCode = `
-            varying mediump vec4 vColor;
+	_createFragmentShader () {
+		let fragmentShaderCode = `
+			precision mediump float;
+            varying vec4 vColor;
+            varying vec3 vNormal;
+            uniform vec3 uLightConstant;
+            uniform float uAmbientIntensity;
+            
             void main(){
-                gl_FragColor = vColor;
+                vec3 ambient = uLightConstant * uAmbientIntensity;
+                vec3 phong = ambient;
+                gl_FragColor = vec4(phong.x * vColor.x, phong.y * vColor.y, phong.z * vColor.z, vColor.w);
             }`
 
-        let fragmentShader = this.context.createShader(this.context.FRAGMENT_SHADER)
-        this.context.shaderSource(fragmentShader, fragmentShaderCode)
-        this.context.compileShader(fragmentShader)
+		let fragmentShader = this.context.createShader(this.context.FRAGMENT_SHADER)
+		this.context.shaderSource(fragmentShader, fragmentShaderCode)
+		this.context.compileShader(fragmentShader)
 
-        return fragmentShader
-    }
+		let compiled = this.context.getShaderParameter(fragmentShader, this.context.COMPILE_STATUS)
+		if (!compiled) {
+			console.error(this.context.getShaderInfoLog(fragmentShader))
+		}
+		return fragmentShader
+	}
 
-    _bindArrayInsideShader(arrayToBePushed, shaderAttribute) {
-        let buffer = this.context.createBuffer();
-        this.context.bindBuffer(this.context.ARRAY_BUFFER, buffer)
-        this.context.bufferData(this.context.ARRAY_BUFFER, arrayToBePushed, this.context.STATIC_DRAW)
+	_bindArrayInsideShader (arrayToBePushed, shaderAttribute) {
+		let buffer = this.context.createBuffer()
+		this.context.bindBuffer(this.context.ARRAY_BUFFER, buffer)
+		this.context.bufferData(this.context.ARRAY_BUFFER, arrayToBePushed, this.context.STATIC_DRAW)
 
-        let coordinate = this.context.getAttribLocation(this.shaderProgram, shaderAttribute)
-        this.context.vertexAttribPointer(coordinate, 3, this.context.FLOAT, false, 0, 0);
-        this.context.enableVertexAttribArray(coordinate)
-    }
+		let coordinate = this.context.getAttribLocation(this.shaderProgram, shaderAttribute)
+		this.context.vertexAttribPointer(coordinate, 3, this.context.FLOAT, false, 0, 0)
+		this.context.enableVertexAttribArray(coordinate)
+	}
 
-    add(geometry) {
-        this.geometries.push(geometry)
-    }
+	_bindUniformArrayInsideShader (arrayToBePushed, shaderUniform) {
+		let uniform = this.context.getUniformLocation(this.shaderProgram, shaderUniform)
+		this.context.uniform3fv(uniform, arrayToBePushed)
+	}
 
-    remove(removedGeometry) {
-        this.geometries.forEach((geometry, index, object) => {
-            if (removedGeometry === geometry) object.splice(index, 1);
-        })
-    }
+	_bindUniformDataInsideShader (valueTobePushed, shaderUniform) {
+		let uniform = this.context.getUniformLocation(this.shaderProgram, shaderUniform)
+		this.context.uniform1f(uniform, valueTobePushed)
+	}
 
-    render() {
-        let vertices = [];
-        let colors = [];
+	add (geometry) {
+		this.geometries.push(geometry)
+	}
 
+	remove (removedGeometry) {
+		this.geometries.forEach((geometry, index, object) => {
+			if (removedGeometry === geometry) object.splice(index, 1)
+		})
+	}
 
-        this.geometries.forEach((geometry) => {
-            vertices.push(...geometry.getVerticeArray())
-            colors.push(...geometry.getColorArray())
-        })
+	render () {
+		let vertices = []
+		let colors = []
+		let normals = []
 
+		this.geometries.forEach((geometry) => {
+			vertices.push(...geometry.getVerticeArray())
+			colors.push(...geometry.getColorArray())
+			normals.push(...geometry.getNormalArray())
+		})
 
-        vertices = new Float32Array([...vertices])
-        colors = new Float32Array([...colors])
+		vertices = new Float32Array([...vertices])
+		colors = new Float32Array([...colors])
 
+		this.context.enable(this.context.DEPTH_TEST)
+		this.context.depthFunc(this.context.LEQUAL)
+		this.context.clearColor(1.0, 1.0, 1.0, 1.0)
+		this.context.clearDepth(1.0)
+		this.context.clear(this.context.COLOR_BUFFER_BIT)
+		this.context.viewport(0, 0, this.domElement.width, this.domElement.height)
 
-        this.context.enable(this.context.DEPTH_TEST);
-        this.context.depthFunc(this.context.LEQUAL);
-        this.context.clearColor(1.0, 1.0, 1.0, 1.0)
-        this.context.clearDepth(1.0)
-        this.context.clear(this.context.COLOR_BUFFER_BIT);
-        this.context.viewport(0, 0, this.domElement.width, this.domElement.height);
+		this.context.enable(this.context.BLEND)
+		this.context.blendFunc(
+			this.context.ONE,
+			this.context.ONE_MINUS_SRC_ALPHA
+		)
 
-        this.context.enable(this.context.BLEND);
-        this.context.blendFunc(
-            this.context.ONE,
-            this.context.ONE_MINUS_SRC_ALPHA
-        );
+		let projection_matrix = this.get_projection(30, this.domElement.width / this.domElement.height, 1, 100)
+		let PMatrixPointer = this.context.getUniformLocation(this.shaderProgram, 'uProjectionMatrix')
+		this.context.uniformMatrix4fv(PMatrixPointer, false, projection_matrix)
 
+		let viewMatrix = [
+			1, 0, 0, 0,
+			0, 1, 0, 0,
+			0, 0, 1, 0,
+			0, 0, 0, 1
+		]
+		viewMatrix[14] = viewMatrix[14] - 3
+		let VMatrixPointer = this.context.getUniformLocation(this.shaderProgram, 'uViewMatrix')
+		this.context.uniformMatrix4fv(VMatrixPointer, false, viewMatrix)
 
-        let projection_matrix = this.get_projection(30, this.domElement.width / this.domElement.height, 1, 100);
-        let PMatrixPointer = this.context.getUniformLocation(this.shaderProgram, "uProjectionMatrix")
-        this.context.uniformMatrix4fv(PMatrixPointer, false, projection_matrix)
+		this._bindArrayInsideShader(vertices, 'aCoordinates')
+		this._bindArrayInsideShader(colors, 'aColors')
+		this._bindArrayInsideShader(normals, 'aNormal')
+		this._bindUniformArrayInsideShader([1, 1, 1], 'uLightConstant')
+		this._bindUniformDataInsideShader(.414, 'uAmbientIntensity')
 
-        let viewMatrix = [
-            1, 0, 0, 0,
-            0, 1, 0, 0,
-            0, 0, 1, 0,
-            0, 0, 0, 1
-        ];
-        viewMatrix[14] = viewMatrix[14] - 3;
-        let VMatrixPointer = this.context.getUniformLocation(this.shaderProgram, "uViewMatrix")
-        this.context.uniformMatrix4fv(VMatrixPointer, false, viewMatrix)
+		this.context.drawArrays(this.context.TRIANGLES, 0, vertices.length / 3)
+	}
 
-        this._bindArrayInsideShader(vertices, "aCoordinates")
-        this._bindArrayInsideShader(colors, "aColors")
-        this.context.drawArrays(this.context.TRIANGLES, 0, vertices.length / 3);
-    }
-
-    get_projection(angle, a, zMin, zMax) {
-        let ang = Math.tan((angle * .5) * Math.PI / 180);//angle*.5
-        return [
-            0.5 / ang, 0, 0, 0,
-            0, 0.5 * a / ang, 0, 0,
-            0, 0, -(zMax + zMin) / (zMax - zMin), -1,
-            0, 0, (-2 * zMax * zMin) / (zMax - zMin), 0
-        ];
-    }
-
+	get_projection (angle, a, zMin, zMax) {
+		let ang = Math.tan((angle * .5) * Math.PI / 180)//angle*.5
+		return [
+			0.5 / ang, 0, 0, 0,
+			0, 0.5 * a / ang, 0, 0,
+			0, 0, -(zMax + zMin) / (zMax - zMin), -1,
+			0, 0, (-2 * zMax * zMin) / (zMax - zMin), 0
+		]
+	}
 
 }
